47ec89723791d329e2f3ec5a32789cea1477c8f3
Tyler Calkins <atcalkins@outlook.com>
2017-08-29 11:37:02 -0400

    created and migrated book model

---
+++ db/migrate/20170829153430_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :description
+      t.string :url
+      t.string :genre
+
+      t.timestamps
+    end
+  end
+end
