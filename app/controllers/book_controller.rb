f0c4414e7a112f5f9829ce0bb8d1e09e6c2859d6
Tyler Calkins <atcalkins@outlook.com>
2017-08-29 11:47:18 -0400

    added form partial template and worked on actions in controller

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,10 +1,25 @@
 class BookController < ApplicationController
   def index
+    @books = Book.all
+  end
 
+  def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+
+      respond_to do |format|
+    if @event.save
+      format.html { redirect_to @book, notice: 'Book was successfully created.' }
+      format.json { render :show, status: :created, location: @book }
+  else
+    format.html { render :new }
+    format.json { render json: @book.errors, status: :unprocessable_entity }
   end
+end
+end
 
   def update
   end
@@ -13,5 +28,6 @@ class BookController < ApplicationController
   end
 
   def destroy
+
   end
 end
