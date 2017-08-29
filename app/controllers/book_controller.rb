7fc793b19a0b636f751bcdadf571e7beb34177ff
Tyler Calkins <atcalkins@outlook.com>
2017-08-29 11:51:37 -0400

    updated book controller actions

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -10,24 +10,52 @@ class BookController < ApplicationController
   def create
     @book = Book.new(book_params)
 
-      respond_to do |format|
-    if @event.save
-      format.html { redirect_to @book, notice: 'Book was successfully created.' }
-      format.json { render :show, status: :created, location: @book }
-  else
-    format.html { render :new }
-    format.json { render json: @book.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
   end
-end
-end
 
   def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
   end
 
   def destroy
-
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def book_params
+    params.require(:book).permit(:title, :author, :description :genre: :url)
+    #requiring the hash name to be event, and the columns that will pass through
+  end
+end
+
+
 end
