8e93e88edeaa87b7a80444d30bc29888c927001f
Tyler Calkins <atcalkins@outlook.com>
2017-08-30 11:39:54 -0400

    added validation to require fields in form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :description, presence: true
+  validates :genre, presence: true
+  validates :url, presence: true
 end
